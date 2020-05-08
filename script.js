dc6e2be69f9e99c017a3f86025cdc482f5b1fab6
Tingting Chen <tingtingmacpro@Tingtings-MacBook-Pro.local>
2020-05-08 00:20:30 -0700

    forecast display done

--- script.js
+++ script.js
@@ -31,8 +31,18 @@ function addCity() {
 
 $("#run-search").on("click",function(event){
     event.preventDefault();
+
     addCity();
     storeCities();
+
+
+    $("#day1").empty();
+    $("#day2").empty();
+    $("#day3").empty();
+    $("#day4").empty();
+    $("#day5").empty();
+
+
     var city = $("#cityName").val();
     // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=430c1d68fb49da82de0adb57f3d4371a&units=imperial&mode=xml";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=430c1d68fb49da82de0adb57f3d4371a&units=imperial";
@@ -41,7 +51,6 @@ $("#run-search").on("click",function(event){
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
 
       var lon = response.coord.lon;
       var lat = response.coord.lat;
@@ -60,8 +69,6 @@ $("#run-search").on("click",function(event){
         url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=430c1d68fb49da82de0adb57f3d4371a",
         method: "GET"
       }).then(function(response) {
-        console.log(response);
-        console.log(response.value);
         $("#uv").text("UV Index: ");
         $("#uvIndex").text(response.value);
         if (response.value >= 10){
@@ -80,13 +87,87 @@ $("#run-search").on("click",function(event){
 
 
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=430c1d68fb49da82de0adb57f3d4371a",
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=430c1d68fb49da82de0adb57f3d4371a&units=imperial",
       method: "GET"
     }).then(function(response) {
-      console.log(response);
-    });
 
-    
+      console.log(response);
+      var iconLink0 = "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png";
+      var iconImg0 = $("<img>");
+      iconImg0.attr("src", iconLink0);
+      $("#title").text("5-Day Forecast");
+      $("#day1").append("<br>");
+      $("#day1").append(response.list[0].dt_txt.slice(0,10));
+      $("#day1").append("<br><br>");
+      $("#day1").append(iconImg0);
+      $("#day1").append("<br><br>");
+      $("#day1").append("Temp: " + response.list[0].main.temp + "\xB0" + "F");
+      $("#day1").append("<br><br>");
+      $("#day1").append("Humidity: " + response.list[0].main.humidity + "%");
+      $("#day1").append("<br><br>");
+
+
+
+    var iconLink8 = "http://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png";
+    var iconImg8 = $("<img>");
+    iconImg8.attr("src", iconLink8);
+    $("#title").text("5-Day Forecast");
+    $("#day2").append("<br>");
+    $("#day2").append(response.list[8].dt_txt.slice(0,10));
+    $("#day2").append("<br><br>");
+    $("#day2").append(iconImg8);
+    $("#day2").append("<br><br>");
+    $("#day2").append("Temp: " + response.list[8].main.temp + "\xB0" + "F");
+    $("#day2").append("<br><br>");
+    $("#day2").append("Humidity: " + response.list[8].main.humidity + "%");
+    $("#day2").append("<br><br>");
+
+
+    var iconLink16 = "http://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png";
+    var iconImg16 = $("<img>");
+    iconImg16.attr("src", iconLink16);
+    $("#title").text("5-Day Forecast");
+    $("#day3").append("<br>");
+    $("#day3").append(response.list[16].dt_txt.slice(0,10));
+    $("#day3").append("<br><br>");
+    $("#day3").append(iconImg16);
+    $("#day3").append("<br><br>");
+    $("#day3").append("Temp: " + response.list[16].main.temp + "\xB0" + "F");
+    $("#day3").append("<br><br>");
+    $("#day3").append("Humidity: " + response.list[16].main.humidity + "%");
+    $("#day3").append("<br><br>");
+
+
+    var iconLink24 = "http://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png";
+    var iconImg24 = $("<img>");
+    iconImg24.attr("src", iconLink24);
+    $("#title").text("5-Day Forecast");
+    $("#day4").append("<br>");
+    $("#day4").append(response.list[24].dt_txt.slice(0,10));
+    $("#day4").append("<br><br>");
+    $("#day4").append(iconImg24);
+    $("#day4").append("<br><br>");
+    $("#day4").append("Temp: " + response.list[24].main.temp + "\xB0" + "F");
+    $("#day4").append("<br><br>");
+    $("#day4").append("Humidity: " + response.list[24].main.humidity + "%");
+    $("#day4").append("<br><br>");
+
+
+    var iconLink32 = "http://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png";
+    var iconImg32 = $("<img>");
+    iconImg32.attr("src", iconLink32);
+    $("#title").text("5-Day Forecast");
+    $("#day5").append("<br>");
+    $("#day5").append(response.list[32].dt_txt.slice(0,10));
+    $("#day5").append("<br><br>");
+    $("#day5").append(iconImg32);
+    $("#day5").append("<br><br>");
+    $("#day5").append("Temp: " + response.list[32].main.temp + "\xB0" + "F");
+    $("#day5").append("<br><br>");
+    $("#day5").append("Humidity: " + response.list[32].main.humidity + "%");
+    $("#day5").append("<br><br>");
+
+  });
 
 });
 
